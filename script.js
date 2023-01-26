f9fe716097df2d185833a186cfc1f8efc6b5c7cf
Josh prog64 <105669045+Josh-prog63@users.noreply.github.com>
2023-01-26 18:09:35 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,97 @@
+// active class to link
+const navUL = document.getElementById("nav-ul");
+const navLi = navUL.getElementsByClassName("nav-li");
+
+for (let i = 0; i < navLi.length; i++) {
+      navLi[i].addEventListener("click", function() {
+      const current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+
+  // Slide content section on click
+  let links = document.querySelectorAll(".link");
+  let allContent = document.querySelectorAll(".content");
+  
+  if (window.innerWidth > 850) {
+      for (let i = 0; i < links.length; i++) {
+          links[i].addEventListener("click", (e) => {
+            e.preventDefault();
+
+            const filter = e.target.dataset.filter;
+            console.log(filter);
+
+            allContent.forEach((content) => {
+              if (filter === "content") {
+                content.style.display = "block"
+              } else if (content.classList.contains(filter)) {
+                content.style.display = "block"
+              } else {
+                content.style.display = "none"
+              }
+
+            })
+          });
+      }
+  }
+
+  // Smooth scroll for tablet
+  if (window.innerWidth < 850) {
+    links = document.querySelectorAll(".nav-li a");
+
+    for (const link of links) {
+      link.addEventListener("click", smoothScroll);
+    }
+
+    function smoothScroll(e) {
+      e.preventDefault();
+      const href = this.getAttribute("href");
+
+      document.querySelector(href).scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+
+
+
+  }
+
+
+
+
+// text effect and text array
+  const dataText = ["Full-stack WebDeveloper", "Blogger", "Programmer", "Green-White Hat", "Cyber-Sec-Associate"];
+  document.addEventListener("DOMContentLoaded", typingEffect);
+
+  function typingEffect() {
+
+  function typeWriter(text, i, fnCallback) {
+    if (i < (text.length)) {
+      document.querySelector(".text-effect").innerHTML = text.substring(0, i+1) + "<span class='effect'></span>";
+
+      // Set delay b4 nxt xter
+      setTimeout(function() {
+        typeWriter(text, i+1, fnCallback)
+      }, 100)
+    } else if (typeof fnCallback == "function") {
+      setTimeout(fnCallback, 700);
+    } 
+  }
+
+  // START Typing
+  function startTextAnimation(i) {
+    if (typeof dataText[i] == "undefined") {
+      setTimeout(function() {
+        startTextAnimation(0);
+      }, 2000)
+    }
+    if (i < dataText[i].length) {
+      typeWriter(dataText[i], 0, function() {
+        startTextAnimation(i + 1);
+      });
+    }
+    
+  }
+  startTextAnimation(0);
+}
